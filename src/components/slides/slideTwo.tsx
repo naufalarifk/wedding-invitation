ceb5ce13dbf7f216d2fdd5267581fe8493b53f34
naufal <abnormalchild414@gmail.com>
2024-01-20 18:18:15 +0700

    chore: fixed date text

--- src/components/slides/slideTwo.tsx
+++ src/components/slides/slideTwo.tsx
@@ -5,8 +5,8 @@ export const SlideTwo = () => {
 
   return (
     <div>
-      <h1><span className="text-2xl font-bold">Minggu</span><span className="text-xl">, 24 Januari 2024</span></h1>
-<CountdownTimer/>
+      <h1 className="mx-auto"><span className="text-2xl font-bold">Minggu</span><span className="text-xl">, 24 Januari 2024</span></h1>
+        <CountdownTimer/>
     </div>
   )
 }
