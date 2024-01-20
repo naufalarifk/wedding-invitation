39517fe1a5b7970eb92355a02a879b053111a27a
naufal <abnormalchild414@gmail.com>
2024-01-20 18:27:15 +0700

    chore: fixed wrong date

--- src/components/slides/slideTwo.tsx
+++ src/components/slides/slideTwo.tsx
@@ -5,7 +5,7 @@ export const SlideTwo = () => {
 
   return (
     <div>
-      <h1 className="mx-auto"><span className="text-2xl font-bold">Minggu</span><span className="text-xl">, 24 Januari 2024</span></h1>
+      <h1 className="mx-auto"><span className="text-2xl font-bold">Minggu</span><span className="text-xl">, 21 Januari 2024</span></h1>
         <CountdownTimer/>
     </div>
   )
